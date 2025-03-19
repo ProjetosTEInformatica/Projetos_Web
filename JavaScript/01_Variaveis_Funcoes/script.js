c389b3acb51497042b87ae7e977c70ca0586df58
Erick Padilha Educador de Informática <labinformaticate@gmail.com>
2025-03-19 15:27:38 -0300

    Adicionando exemplos a pasta JavaScript

---
+++ JavaScript/01_Variaveis_Funcoes/script.js
@@ -0,0 +1,16 @@
+// Variáveis
+let nome = "Educador";
+let idade = 17;
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+
+// Função Simples
+function mostrarMensagem() {
+    alert("Olá, " + nome + "! Bem-vinda ao JavaScript!");
+}
+
+// Função com Retorno
+function somar(a, b) {
+    return a + b;
+}
+console.log("Soma de 5 + 3 =", somar(5, 3));
